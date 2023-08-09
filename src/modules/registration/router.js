cf86534e26cde241b32043aa9b981168485eeefd
najibean <najibullah89@gmail.com>
2023-08-09 21:49:23 +0800

    update crud user

--- src/modules/registration/router.js
+++ src/modules/registration/router.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
+const UserController = require('./controllers/user.controller');
 const router = Router();
 
 router.get('/test', (req, res) => {
   res.send('test registration');
 });
+
+router.get('/user', UserController.getAll);
+router.get('/user/:id', UserController.getOne);
+router.post('/user', UserController.create);
+router.patch('/user/:id', UserController.update);
+router.delete('/user/:id', UserController.delete);
 
 module.exports = router;
