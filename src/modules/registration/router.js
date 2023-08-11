ad71b966f33379a577cfe8c5459a3f69836d4b7a
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-08-11 14:28:34 +0700

    add Teacher Routes

--- src/modules/registration/router.js
+++ src/modules/registration/router.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const UserController = require('./controllers/user.controller');
+const PengajarController = require('./controllers/teacher.controller');
 const router = Router();
 
 router.get('/test', (req, res) => {
@@ -11,5 +12,12 @@ router.get('/user/:id', UserController.getOne);
 router.post('/user', UserController.create);
 router.patch('/user/:id', UserController.update);
 router.delete('/user/:id', UserController.delete);
+
+// Teacher Route
+router.get('/pengajar', PengajarController.getAll);
+router.get('/pengajar/:id', PengajarController.getOne);
+router.post('/pengajar', PengajarController.create);
+router.patch('/pengajar/:id', PengajarController.update);
+router.delete('/pengajar', PengajarController.delete);
 
 module.exports = router;
