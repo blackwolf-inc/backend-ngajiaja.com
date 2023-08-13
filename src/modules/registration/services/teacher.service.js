14524fe640d6585204d5667f7d89a09aa55cf74c
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-08-13 23:28:22 +0700

    fix fastest-validation user input

--- src/modules/registration/services/teacher.service.js
+++ src/modules/registration/services/teacher.service.js
@@ -8,35 +8,43 @@ class TeacherService extends BaseService {
   async checkUser(id) {
     const result = await User.findOne({ where: { id } });
     if (!result) throw ApiError.notFound('User not found');
+
+    return result;
   }
 
   async checkTeacherDuplicate(id) {
     const result = await Pengajar.findOne({ where: { user_id: id } });
     if (result) throw ApiError.badRequest('Data duplicated');
+
+    return result;
   }
 
   async inputValidation(req) {
     const schema = {
       user_id: { type: 'number' },
       pendidikan_terakhir: { type: 'string', optional: false },
-      punya_sertifikasi_guru_quran: { type: 'number', enum: [0, 1] },
+      punya_sertifikasi_guru_quran: { type: 'enum', values: [0, 1] },
       background_pendidikan_quran: { type: 'string', optional: false },
       pengalaman_mengajar: { type: 'string', optional: false },
-      pernah_mengajar_online: { type: 'number', enum: [0, 1] },
-      paham_aplikasi_meet: { type: 'number', enum: [0, 1] },
-      hafalan_quran: { type: 'string', optional: z },
-      siap_komitmen: { type: 'number', enum: [0, 1] },
+      pernah_mengajar_online: { type: 'enum', values: [0, 1] },
+      paham_aplikasi_meet: { type: 'enum', values: [0, 1] },
+      hafalan_quran: { type: 'string', optional: false },
+      siap_komitmen: { type: 'enum', values: [0, 1] },
       jam_mengajar: { type: 'string', optional: false },
-      mengajar_hari_libur: { type: 'number', enum: [0, 1] },
-      bagi_hasil_50persen: { type: 'number', enum: [0, 1] },
+      mengajar_hari_libur: { type: 'enum', values: [0, 1] },
+      bagi_hasil_50persen: { type: 'enum', values: [0, 1] },
     };
 
     const v = new Validator();
     const validation = v.compile(schema);
 
     const validationResult = validation(req.body);
+    console.log('debbug');
+    console.log(validationResult);
 
-    if (!validationResult) return validationResult;
+    if (validationResult !== true) {
+      return false;
+    }
 
     return true;
   }
@@ -46,7 +54,7 @@ class TeacherService extends BaseService {
       email,
       name,
     });
-    SendEmailNotification.sendMail(email, 'Register Pengajar Notification', getHtml);
+    return SendEmailNotification.sendMail(email, 'Register Pengajar Notification', getHtml);
   }
 }
 
