1ddb3ff5d58e091697291ad509ec69c4be34c69e
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-08-13 21:47:09 +0700

    add validation to table Pengajar

--- src/migrations/20230811050041-create-pengajar.js
+++ src/migrations/20230811050041-create-pengajar.js
@@ -6,58 +6,98 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       pendidikan_terakhir: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       punya_sertifikasi_guru_quran: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       background_pendidikan_quran: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       pengalaman_mengajar: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       pernah_mengajar_online: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       paham_aplikasi_meet: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       hafalan_quran: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       siap_komitmen: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       jam_mengajar: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       mengajar_hari_libur: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       bagi_hasil_50persen: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       isVerifiedByAdmin: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          isIn: [[0, 1]],
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Pengajars');
-  }
+  },
 };
