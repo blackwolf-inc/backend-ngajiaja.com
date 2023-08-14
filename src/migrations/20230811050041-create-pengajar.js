8fdd0b74c90ea674028ac3e60afa99ff9cbe290f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-08-14 08:34:38 +0700

    modify validation

--- src/migrations/20230811050041-create-pengajar.js
+++ src/migrations/20230811050041-create-pengajar.js
@@ -10,55 +10,42 @@ module.exports = {
       },
       user_id: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       pendidikan_terakhir: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       lembaga_pendidikan_terakhir: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       punya_sertifikasi_guru_quran: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       pengalaman_mengajar: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       pernah_mengajar_online: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       paham_aplikasi_meet: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       hafalan_quran: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       siap_komitmen: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       jam_mengajar: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       mengajar_hari_libur: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       bagi_hasil_50persen: {
         type: Sequelize.INTEGER,
-        allowNull: false,
       },
       isVerifiedByAdmin: {
         type: Sequelize.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       createdAt: {
--- src/models/pengajar.js
+++ src/models/pengajar.js
@@ -19,74 +19,61 @@ module.exports = (sequelize, DataTypes) => {
     {
       user_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         unique: true,
       },
       pendidikan_terakhir: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       lembaga_pendidikan_terakhir: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       punya_sertifikasi_guru_quran: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         validate: {
           isIn: [[0, 1]],
         },
       },
       pengalaman_mengajar: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       pernah_mengajar_online: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         validate: {
           isIn: [[0, 1]],
         },
       },
       paham_aplikasi_meet: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         validate: {
           isIn: [[0, 1]],
         },
       },
       hafalan_quran: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       siap_komitmen: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         validate: {
           isIn: [[0, 1]],
         },
       },
       jam_mengajar: {
         type: DataTypes.STRING,
-        allowNull: false,
       },
       mengajar_hari_libur: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         validate: {
           isIn: [[0, 1]],
         },
       },
       bagi_hasil_50persen: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         validate: {
           isIn: [[0, 1]],
         },
       },
       isVerifiedByAdmin: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
         validate: {
           isIn: [[0, 1]],
