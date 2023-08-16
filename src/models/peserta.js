027791ba4a5225bd9791c51e5a60c9eb107a2641
Ferdie12 <maulanaferdie1@gmail.com>
2023-08-16 20:27:57 +0700

    feat : fix peserta models

--- src/models/peserta.js
+++ src/models/peserta.js
@@ -18,36 +18,21 @@ module.exports = (sequelize, DataTypes) => {
       profesi: DataTypes.STRING,
       bisa_baca_ayat: {
         type: DataTypes.STRING,
-        validate: {
-          isIn: [
-            [
-              'Belum bisa membaca Quran',
-              'Bisa membaca Quran tapi terbata-bata',
-              'Bisa membaca Quran dengan lancar',
-              'Bisa membaca dan sedang menghafal Quran',
-            ],
-          ],
-        },
       },
       menguasai_ilmu_tajwid: {
         type: DataTypes.INTEGER,
-        validate: { isIn: [[0, 1]] },
       },
       paham_aplikasi_meet: {
         type: DataTypes.INTEGER,
-        validate: { isIn: [[0, 1]] },
       },
       siap_komitmen_mengaji: {
         type: DataTypes.INTEGER,
-        validate: { isIn: [[0, 1]] },
       },
       siap_komitmen_infak: {
         type: DataTypes.INTEGER,
-        validate: { isIn: [[0, 1]] },
       },
       bersedia_bayar_20K: {
         type: DataTypes.INTEGER,
-        validate: { isIn: [[0, 1]] },
       },
     },
     {
