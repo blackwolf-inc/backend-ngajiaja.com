ff9fbd49a9a8a6b654cb015dc586c56edbda4d33
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-09-02 22:29:47 +0700

    create controller for bank

---
+++ src/modules/monetary/controllers/bank.controller.js
@@ -0,0 +1,38 @@
+const BankService = require('../service/bank.service');
+const responseHandler = require('../../../helpers/responseHandler');
+const db = require('../../../models/index');
+const { Bank, sequelize } = db;
+
+class BankController {
+  static async getOne(req, res, next) {
+    const service = new BankService(req, Bank);
+    try {
+      const result = await service.getOneById(req.params.id);
+      return responseHandler.succes(res, `Success get ${service.db.name}`, result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getAll(req, res, next) {
+    const service = new BankService(req, Bank);
+    try {
+      const result = await service.getAll();
+      return responseHandler.succes(res, `Success get all ${service.db.name}s`, result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async create(req, res, next) {
+    const service = new BankService(req, Bank);
+    try {
+      const result = await service.createData(req.body);
+      return responseHandler.succes(res, `Success create ${service.db.name}`, result);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = BankController;
