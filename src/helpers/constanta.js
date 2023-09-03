5f34329db4c4127c02136b8a5f84d37256d9f51e
najibean <najibullah89@gmail.com>
2023-09-03 08:33:28 +0800

    add seeder superadmin, add constanta user rules

---
+++ src/helpers/constanta.js
@@ -0,0 +1,10 @@
+const roleUser = {
+  SUPER_ADMIN: 'SUPER_ADMIN',
+  ADMIN: 'ADMIN',
+  PENGAJAR: 'PENGAJAR',
+  PESERTA: 'PESERTA',
+};
+
+module.exports = {
+  roleUser,
+};
---
+++ src/seeders/20230902233716-super-admin-data-init.js
@@ -0,0 +1,27 @@
+const { getHash } = require('../helpers/passwordHash');
+const { roleUser } = require('./../helpers/constanta');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        role: roleUser.SUPER_ADMIN,
+        nama: 'superadmin',
+        email: 'superadmin.ngajiaja@yopmail.com',
+        telp_wa: '08123456789',
+        jenis_kelamin: 'L',
+        alamat: 'Bandara Mas, Batam',
+        usia: 33,
+        password: getHash('password'),
+        token: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
