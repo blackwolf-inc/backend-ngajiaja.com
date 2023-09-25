2489ede11db98da84d174d3b91fda50aba4360b7
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-09-25 07:22:01 +0700

    modify bank service

--- src/modules/monetary/service/bank.service.js
+++ src/modules/monetary/service/bank.service.js
@@ -1,5 +1,22 @@
 const BaseService = require('../../../base/base.service');
+const ApiError = require('../../../helpers/errorHandler');
+const { Bank } = require('../../../models');
+const { Op, fn } = require('sequelize');
 
-class BankService extends BaseService {}
+class BankService extends BaseService {
+  async checkBankDuplicate(req) {
+    const result = await Bank.findOne({
+      where: {
+        [Op.or]: [{ nama_bank: { [Op.like]: fn('LOWER', `%${req}%`) } }],
+      },
+    });
+
+    if (result) {
+      throw ApiError.badRequest(`Bank with name ${req} already exist`);
+    }
+
+    return result;
+  }
+}
 
 module.exports = BankService;
