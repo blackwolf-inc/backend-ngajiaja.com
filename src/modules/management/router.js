c341f14a45662b4e546c5da9ada0b76fae89107b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-10-11 15:50:05 +0700

    modify routes

--- src/modules/management/router.js
+++ src/modules/management/router.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const JadwalMengajar = require('./controllers/jadwalMengajar.controller');
+const PilihPengajar = require('./controllers/pilihPengajar.controller');
 const router = Router();
 const validate = require('./../../utils/validatorIndex');
 const isAuthenticate = require('./../../middlewares/authentication');
@@ -7,6 +8,12 @@ const {
   createJadwalMengajarValidator,
   updateJadwalValidator,
 } = require('./validators/jadwalPengajar.validator');
+
+const {
+  createPilihPengajarValidator,
+  createTambahanValidator,
+  updatePilihPengajarValidator,
+} = require('./validators/pilihPengajar.validator');
 const { USER_ROLE } = require('../../helpers/constanta');
 const { hasRole } = require('../../middlewares/roleAuth');
 
@@ -36,6 +43,27 @@ router.delete(
   '/jadwal-pengajar/:id',
   hasRole([USER_ROLE.SUPER_ADMIN, USER_ROLE.ADMIN, USER_ROLE.PENGAJAR]),
   JadwalMengajar.delete
+);
+
+// pilih pengajar & buat bimbingan route
+router.get('/bimbingan-pengajar', PilihPengajar.getAll);
+router.post('/bimbingan-pengajar', validate(createPilihPengajarValidator), PilihPengajar.create);
+router.patch(
+  '/bimbingan-pengajar/:id',
+  validate(updatePilihPengajarValidator),
+  PilihPengajar.update
+);
+router.delete('/bimbingan-pengajar/:id', PilihPengajar.delete);
+
+router.post(
+  '/bimbingan-tambahan-pengajar',
+  validate(createTambahanValidator),
+  PilihPengajar.createTambahan
+);
+router.patch(
+  '/bimbingan-tambahan-pengajar/:id',
+  validate(createTambahanValidator),
+  PilihPengajar.createTambahan
 );
 
 module.exports = router;
