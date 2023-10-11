5202263351a5e86a1b51f441782052c7083d1b43
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-10-11 15:49:44 +0700

    create model bimbingan reguler

---
+++ src/models/bimbinganreguler.js
@@ -0,0 +1,36 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class BimbinganReguler extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      BimbinganReguler.belongsTo(models.Period, {
+        foreignKey: 'period_id',
+        as: 'period',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      });
+    }
+  }
+  BimbinganReguler.init(
+    {
+      period_id: DataTypes.INTEGER,
+      tanggal: DataTypes.STRING,
+      hari_bimbingan: DataTypes.STRING,
+      jam_bimbingan: DataTypes.STRING,
+      absensi_peserta: DataTypes.INTEGER,
+      absensi_pengajar: DataTypes.INTEGER,
+      catatan_pengajar: DataTypes.TEXT,
+      tanggal_pengingat_infaq: DataTypes.DATEONLY,
+    },
+    {
+      sequelize,
+      modelName: 'BimbinganReguler',
+    }
+  );
+  return BimbinganReguler;
+};
