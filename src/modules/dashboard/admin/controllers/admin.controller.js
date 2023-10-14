c7505ad58099abac19aeea74e5b6716edfa2d087
najibean <najibullah89@gmail.com>
2023-10-14 11:53:55 +0800

    add service admin dashboard

--- src/modules/dashboard/admin/controllers/admin.controller.js
+++ src/modules/dashboard/admin/controllers/admin.controller.js
@@ -1,16 +1,11 @@
-const dataPengajarService = require('../services/dataPengajar.service');
+const AdminPengajarService = require('../services/adminPengajar.service');
 const responseHandler = require('./../../../../helpers/responseHandler');
 
 class AdminDashboardController {
   static async dataPengajar(req, res, next) {
+    const service = new AdminPengajarService();
     try {
-      const { hariBimbingan1, jamBimbingan1, hariBimbingan2, jamBimbingan2 } = req.body;
-      const result = await dataPengajarService.dataPengajarAll(
-        hariBimbingan1.toUpperCase(),
-        jamBimbingan1,
-        hariBimbingan2.toUpperCase(),
-        jamBimbingan2
-      );
+      const result = await service.getDataPengajar();
       return responseHandler.succes(res, 'Success get data pengajar all', result);
     } catch (error) {
       next(error);
---
+++ src/modules/dashboard/admin/services/adminPengajar.service.js
@@ -0,0 +1,93 @@
+const { QueryTypes } = require('sequelize');
+const db = require('../../../../models/index');
+const { Pengajar, sequelize } = db;
+
+class AdminPengajarService {
+  /**
+   * Seharusnya ini ada di dashboard peserta saat milih pengajar nya.
+   */
+  async pilihPengajarOlehPeserta(hariBimbingan1, jamBimbingan1, hariBimbingan2, jamBimbingan2) {
+    const mulaiMengajar1 = jamBimbingan1.slice(0, 5);
+    const mulaiMengajar2 = jamBimbingan2.slice(0, 5);
+
+    /**
+     * Hasil nya hanya bisa didapat jika kedua schedule terpenuhi.
+     * Jika hanya satu schedule saja maka tidak akan muncul.
+     */
+    const resultJadwal = await Promise.all([
+      sequelize.query(
+        `
+        SELECT 
+          p.id AS 'pengajar_id', 
+          u.id AS 'user_id', u.nama AS 'nama_pengajar', u.status, u.role
+          -- jmp.hari_mengajar, jmp.mulai_mengajar, jmp.selesai_mengajar 
+        FROM JadwalMengajarPengajars jmp 
+        JOIN Pengajars p ON jmp.pengajar_id = p.id 
+        JOIN Users u ON p.user_id = u.id 
+        WHERE 
+          u.role = 'PENGAJAR' AND
+          (jmp.hari_mengajar = '${hariBimbingan1}' AND jmp.mulai_mengajar = '${mulaiMengajar1}') OR 
+          (jmp.hari_mengajar = '${hariBimbingan2}' AND jmp.mulai_mengajar = '${mulaiMengajar2}')
+        GROUP BY p.id 
+        HAVING COUNT(p.id) > 1
+        ORDER BY u.nama, jmp.mulai_mengajar
+        `,
+        { type: QueryTypes.SELECT }
+      ),
+    ]);
+
+    return resultJadwal;
+  }
+
+  async getDataPengajar() {
+    const [total_registered, total_active, total_nonactive, all_pengajars] = await Promise.all([
+      sequelize.query(
+        `
+        SELECT COUNT(*) AS 'total_pengajar'
+        FROM Pengajars p 
+        JOIN Users u ON p.user_id = u.id 
+        WHERE u.role = 'PENGAJAR' AND u.status = 'REGISTERED'
+        `,
+        { type: QueryTypes.SELECT }
+      ),
+      sequelize.query(
+        `
+        SELECT COUNT(*) AS 'total_pengajar'
+        FROM Pengajars p 
+        JOIN Users u ON p.user_id = u.id 
+        WHERE u.role = 'PENGAJAR' AND u.status = 'ACTIVE'
+        `,
+        { type: QueryTypes.SELECT }
+      ),
+      sequelize.query(
+        `
+        SELECT COUNT(*) AS 'total_pengajar'
+        FROM Pengajars p 
+        JOIN Users u ON p.user_id = u.id 
+        WHERE u.role = 'PENGAJAR' AND u.status = 'NONACTIVE'
+        `,
+        { type: QueryTypes.SELECT }
+      ),
+      sequelize.query(
+        `
+        SELECT
+          u.id AS 'user_id', u.nama, u.role, u.status,
+          p.id AS 'Pengajar_id', p.tanggal_wawancara, p.jam_wawancara, p.link_wawancara 
+        FROM Pengajars p 
+        JOIN Users u ON p.user_id = u.id 
+        WHERE u.role = 'PENGAJAR'
+        `,
+        { type: QueryTypes.SELECT }
+      ),
+    ]);
+
+    return {
+      total_registered,
+      total_active,
+      total_nonactive,
+      all_pengajars,
+    };
+  }
+}
+
+module.exports = AdminPengajarService;
