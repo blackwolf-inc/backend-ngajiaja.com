db7359f7ef097b1ddb530ada850a33efa27f7b20
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2023-10-31 17:37:47 +0800

    bug fix add bibingan reguler

--- src/modules/management/service/pilihPengajar.service.js
+++ src/modules/management/service/pilihPengajar.service.js
@@ -76,13 +76,12 @@ class PilihPengajar extends BaseService {
     const dateNow = moment(new Date()).format('YYYY-MM-DD');
     const dateVerifikasi = moment(dateNow).add('days', 3).format('YYYY-MM-DD');
     const dateEnd = moment(dateVerifikasi).endOf('month').format('YYYY-MM-DD');
-    const dateRemember = moment(Date(tanggal_pengingat_infaq)).format('YYYY-MM-DD');
+    const dateRemember = moment(tanggal_pengingat_infaq).format('YYYY-MM-DD');
     const dateDiff = moment(dateEnd).diff(dateVerifikasi, 'days');
 
     let dateThisMonth = dateVerifikasi;
     for (let i = 1; i <= dateDiff; i++) {
       if (hari_1 == moment(dateThisMonth).format('dddd')?.toUpperCase()) {
-        dateThisMonth = moment(dateThisMonth).add('days', 1).format('YYYY-MM-DD');
         const query = {
           period_id: id,
           tanggal_pengingat_infaq: dateRemember,
@@ -92,8 +91,8 @@ class PilihPengajar extends BaseService {
         };
         const result = await BimbinganReguler.create(query);
         records.push(result);
-      } else if (hari_2 == moment(dateThisMonth).format('dddd')?.toUpperCase()) {
         dateThisMonth = moment(dateThisMonth).add('days', 1).format('YYYY-MM-DD');
+      } else if (hari_2 == moment(dateThisMonth).format('dddd')?.toUpperCase()) {
         const query = {
           period_id: id,
           tanggal_pengingat_infaq: dateRemember,
@@ -103,6 +102,7 @@ class PilihPengajar extends BaseService {
         };
         const result = await BimbinganReguler.create(query);
         records.push(result);
+        dateThisMonth = moment(dateThisMonth).add('days', 1).format('YYYY-MM-DD');
       } else {
         dateThisMonth = moment(dateThisMonth).add('days', 1).format('YYYY-MM-DD');
       }
