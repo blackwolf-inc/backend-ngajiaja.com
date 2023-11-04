49dc167481f809ce4196f5663aea4aee3a0c991c
BagusMiftahRizqullah <bagusmiftah9@gmail.com>
2023-11-04 16:49:43 +0800

    delll consoleLog

--- src/modules/monetary/controllers/infaq.controller.js
+++ src/modules/monetary/controllers/infaq.controller.js
@@ -47,7 +47,6 @@ class InfaqController {
     try {
       await service.checkInfaqById(req.params.id);
       const result = await service.updateData(req.body, { id: req.params.id });
-      console.log('REQ BOY UPDATE', result);
       if (result) {
         await service.addToPenghasilanPengajar(result);
         return responseHandler.succes(res, `Success create ${service.db.name}`, result);
