e410f2ac78e219dbe818b6a49a5b7549f53aeb51
Ferdie12 <maulanaferdie1@gmail.com>
2023-11-12 16:49:15 +0700

    feat: add api profile peserta get and put

--- src/modules/dashboard/peserta/router.js
+++ src/modules/dashboard/peserta/router.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const validate = require('./../../../utils/validatorIndex');
 const isAuthenticate = require('./../../../middlewares/authentication');
 const { USER_ROLE } = require('../../../helpers/constanta');
 const { hasRole } = require('../../../middlewares/roleAuth');
 const DashboardPesertaController = require('./controllers/peserta.controller');
+const ProfilePesertaController = require('./controllers/profile-peserta.controllers');
+const validate = require('./../../../utils/validatorIndex');
+const updateProfileValidator = require('./validation/profile-peserta.validator');
+const storageImage = require('../../../utils/storageImage');
 
 router.get('/test', (req, res) => {
   res.status(200).json({
@@ -15,5 +18,12 @@ router.get('/test', (req, res) => {
 router.use(hasRole([USER_ROLE.PESERTA]));
 router.get('/data-bimbingan/:id', DashboardPesertaController.dataBimbingan);
 router.get('/bimbingan-peserta/:id', DashboardPesertaController.bimbinganPeseta);
+router.get('/profile/:id', ProfilePesertaController.getOne);
+router.put(
+  '/profile/:id',
+  storageImage.image.single('media'),
+  validate(updateProfileValidator),
+  ProfilePesertaController.update
+);
 
 module.exports = router;
