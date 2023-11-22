453e69bfb2baa90e0061c367ae2a59737ee33957
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-11-22 19:05:39 +0800

    update migration and model

--- src/migrations/20231011064863-AddColoumBankPengajar.js
+++ src/migrations/20231011064863-AddColoumBankPengajar.js
@@ -1,19 +1,24 @@
 'use strict';
 
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.addColumn('Pengajars', 'nama_bank', {
-            type: Sequelize.STRING, // Tipe data kolom
-            allowNull: true, // Opsional, sesuaikan sesuai kebutuhan
-        }),
-            await queryInterface.addColumn('Pengajars', 'no_bank', {
-                type: Sequelize.STRING, // Tipe data kolom
-                allowNull: true, // Opsional, sesuaikan sesuai kebutuhan
-            })
-    },
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Pengajars', 'nama_bank', {
+      type: Sequelize.STRING, // Tipe data kolom
+      allowNull: true, // Opsional, sesuaikan sesuai kebutuhan
+    });
+    await queryInterface.addColumn('Pengajars', 'no_rekening', {
+      type: Sequelize.STRING, // Tipe data kolom
+      allowNull: true, // Opsional, sesuaikan sesuai kebutuhan
+    });
+    await queryInterface.addColumn('Pengajars', 'nama_rekening', {
+      type: Sequelize.STRING, // Tipe data kolom
+      allowNull: true, // Opsional, sesuaikan sesuai kebutuhan
+    });
+  },
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.removeColumn('Pengajars', 'nama_bank');
-        await queryInterface.removeColumn('Pengajars', 'no_bank');
-    },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Pengajars', 'nama_bank');
+    await queryInterface.removeColumn('Pengajars', 'no_rekening');
+    await queryInterface.removeColumn('Pengajars', 'nama_rekening');
+  },
 };
